fb41cd914988c8bbc7cea6ee487c2ac94206eb24
iamkimlog <iamkimlog@gmail.com>
2023-05-17 18:09:48 +0900

    13.10 변형: 첫 n개 항목 가져오기

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -12,9 +12,9 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({ :ok, %{status_code: 200, body: body }}) do
-    { :ok, body }
-  end
+  # def handle_response({ :ok, %{status_code: 200, body: body }}) do
+  #   { :ok, body }
+  # end
 
   def handle_response({ _, %{status_code: status_code, body: body}}) do
     {
