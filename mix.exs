f2b34634bc079df5f6f722c47323715dfdcae8c0
iamkimlog <iamkimlog@gmail.com>
2023-05-23 17:09:16 +0900

    14.2.4 테스트 커버리지

--- mix.exs
+++ mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.MixProject do
       start_permanent: Mix.env() == :prod,
       name: "Issues",
       source_url: "https://github.com/iamkimlog/elixir-issues",
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -29,7 +36,8 @@ defmodule Issues.MixProject do
       { :httpoison, "~> 1.0" },
       { :poison, "~> 5.0" },
       { :ex_doc, "~> 0.25" },
-      { :earmark, "~> 1.4" }
+      { :earmark, "~> 1.4" },
+      { :excoveralls, "~> 0.14", only: :test }
     ]
   end
 
