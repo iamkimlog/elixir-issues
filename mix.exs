3b776457b108c33fc5a018818893d54bee6abb24
iamkimlog <iamkimlog@gmail.com>
2023-05-23 15:43:44 +0900

    13.14 5단계: 프로젝트 문서 생성하기

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Issues.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      name: "Issues",
+      source_url: "https://github.com/iamkimlog/elixir-issues",
       deps: deps()
     ]
   end
@@ -26,6 +28,8 @@ defmodule Issues.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       { :httpoison, "~> 1.0" },
       { :poison, "~> 5.0" },
+      { :ex_doc, "~> 0.25" },
+      { :earmark, "~> 1.4" }
     ]
   end
 
